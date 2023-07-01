21b8448c7954816d7f7964091841b8a0dcf6c4b1
Azikenp <azikenp26@gmail.com>
2023-07-01 11:23:22 +0100

    customizing event when user clicks map

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+
+
 //Using geolocation API
+
 navigator.geolocation.getCurrentPosition(function(position){
     const latitude = position.coords.latitude;
     const {longitude} = position.coords;
@@ -20,14 +23,29 @@ navigator.geolocation.getCurrentPosition(function(position){
     const coords = [latitude, longitude];
     //integrating leaflet
     const map = L.map('map').setView(coords, 13);
-
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+
+    map.on('click', function(mapEvent){
+        console.log(mapEvent);
+
+        const {lat, lng} = mapEvent.latlng;
+
+        L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                miWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+            })
+            )
+            .setPopupContent('Workout')
+            .openPopup();
+    })
 
     },
     function(){
