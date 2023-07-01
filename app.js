5ee50fbadcb260b4aeb7085dd780787ec1af2044
Azikenp <azikenp26@gmail.com>
2023-07-01 07:38:21 +0100

    integrating leaflet

--- app.js
+++ app.js
@@ -17,6 +17,19 @@ navigator.geolocation.getCurrentPosition(function(position){
     const {longitude} = position.coords;
     console.log(`https://www.google.com/maps/@${latitude},${longitude},12z?entry=ttu`);
 
-}, function(){
+    const coords = [latitude, longitude];
+    //integrating leaflet
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(coords).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+
+    },
+    function(){
     alert('Could not get your position')
 })
